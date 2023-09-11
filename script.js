7f7a565a70dfdd45bf105885e2d2f8ce6480ec27
Franco Saltiba <114366914+azulkhadwhitme@users.noreply.github.com>
2023-09-11 02:46:11 -0300

    cambio computadora

--- script.js
+++ script.js
@@ -85,14 +85,3 @@ introButtons.forEach((button, index) => {
     });
 });
 
-const fulImgBox = document.getElementById("fulImgBox"),
-fulImg = document.getElementById("fulImg");
-
-function openFulImg(reference){
-    fulImgBox.style.display = "flex";
-    fulImg.src = reference
-}
-function closeImg(){
-    fulImgBox.style.display = "none";
-}
-
